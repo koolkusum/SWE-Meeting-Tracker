019718575894240b0d8ab42a3406583246db1aed
Daniel Zhang <danqzbusiness@gmail.com>
2023-03-25 18:29:14 -0400

    attendance count++

--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 session_start();
 
 	include("connection.php");
-	include("functions.php");
+	include("functions.php");	
+	include("sessionInfo.php");
+
 
 	$user_data = check_login($con);
 
+    if(isset($_POST['submit'])) {
+
+        $id = 3; // The ID of the row to update
+    	$query = "SELECT attendances FROM users WHERE user_name = '$user_netID'";
+		$new_num_of_attendances = mysqli_query($con, $query);
+        
+		//update 
+		$new_attendance_count =
+    	$query = "UPDATE users SET attendances = $new_attendance_count WHERE user_name = '$user_name'";
+
+        mysqli_query($con, $query);
+        echo "attendances++";
+      }
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +30,9 @@ session_start();
 </head>
 <body>
 
-	<a href="updateEvent.php">New Event</a>
+	<form method="post">
+        <input type="submit" name="submit" value="I am currently attending event">
+    </form>
 	<a href="logout.php">Logout</a>
 	<h1>This is the index page</h1>
 
