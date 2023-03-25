06c2d1f513b437019e3cfe2be2a10336937e92c8
Daniel Zhang <danqzbusiness@gmail.com>
2023-03-25 17:22:17 -0400

    asdf

--- updateEvent.php
+++ updateEvent.php
@@ -1,5 +1,8 @@
 <?php 
 session_start();
+    include("connection.php");	
+    include("functions.php");
+
 
     $query = "UPDATE users SET attended_current_event = 0 WHERE attended_current_event = 1"
     mysqli_query($con, $query);
