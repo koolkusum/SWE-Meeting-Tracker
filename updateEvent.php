7702d924a82c08a4c6bf45d33838f05f04918aa1
Daniel Zhang <danqzbusiness@gmail.com>
2023-03-25 17:14:55 -0400

    asdf

--- updateEvent.php
+++ updateEvent.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
 
-    $query = "UPDATE users SET attended_current_event = '0' WHERE attended_current_event = '1'"
+    $query = "UPDATE users SET attended_current_event = 0 WHERE attended_current_event = 1"
 
 ?>
