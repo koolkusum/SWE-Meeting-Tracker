a898ec58d0f9142fdc2780709f23756f19e63df6
Daniel Zhang <danqzbusiness@gmail.com>
2023-03-25 22:12:03 -0400

    ass

--- admin.php
+++ admin.php
@@ -15,42 +15,6 @@ if (isset($_POST['submit2'])) {
     mysqli_query($con, $query);
 }
 
-$query = "SELECT COUNT(*) as num_users FROM users";
-$result = mysqli_query($con, $query);
-$row = mysqli_fetch_assoc($result);
-$memberCount = $row['num_users'];
-
-for ($i = 0; $i < $memberCount; $i++) {
-
-    // Combine first and last name
-    $query = "SELECT first_name FROM users LIMIT $i, 1";
-    $result = mysqli_query($con, $query);
-    $firstName = mysqli_fetch_assoc($result)['first_name'];
-    $query = "SELECT last_name FROM users LIMIT $i, 1";
-    $result = mysqli_query($con, $query);
-    $lastName = mysqli_fetch_assoc($result)['last_name'];
-    $memberName = "$firstName $lastName";
-
-    // NetID, attendance count, major, grad year
-    $query = "SELECT user_name FROM users LIMIT $i, 1";
-    $result = mysqli_query($con, $query);
-    $memberNetID = mysqli_fetch_assoc($result)['user_name'];
-
-    $query = "SELECT attendances FROM users LIMIT $i, 1";
-    $result = mysqli_query($con, $query);
-    $attendanceCount = mysqli_fetch_assoc($result)['attendances'];
-
-    $query = "SELECT graduation_year FROM users LIMIT $i, 1";
-    $result = mysqli_query($con, $query);
-    $gradYear = mysqli_fetch_assoc($result)['graduation_year'];
-
-    $query = "SELECT major FROM users LIMIT $i, 1";
-    $result = mysqli_query($con, $query);
-    $major = mysqli_fetch_assoc($result)['major'];
-
-    echo "<p> $memberName\n NetID: $memberNetID\n attendances: $attendanceCount\n Major: $major\n grad year: $gradYear\n attendances: $attendanceCount\n\n</p>";
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -61,6 +25,54 @@ for ($i = 0; $i < $memberCount; $i++) {
 </head>
 
 <body>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Member Name</th>
+                <th>Attendance Count</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $query = "SELECT COUNT(*) as num_users FROM users";
+            $result = mysqli_query($con, $query);
+            $row = mysqli_fetch_assoc($result);
+            $memberCount = $row['num_users'];
+            
+            for ($i = 0; $i < $memberCount; $i++) {
+            
+                // Combine first and last name
+                $query = "SELECT first_name FROM users LIMIT $i, 1";
+                $result = mysqli_query($con, $query);
+                $firstName = mysqli_fetch_assoc($result)['first_name'];
+                $query = "SELECT last_name FROM users LIMIT $i, 1";
+                $result = mysqli_query($con, $query);
+                $lastName = mysqli_fetch_assoc($result)['last_name'];
+                $memberName = "$firstName $lastName";
+            
+                // NetID, attendance count, major, grad year
+                $query = "SELECT user_name FROM users LIMIT $i, 1";
+                $result = mysqli_query($con, $query);
+                $memberNetID = mysqli_fetch_assoc($result)['user_name'];
+            
+                $query = "SELECT attendances FROM users LIMIT $i, 1";
+                $result = mysqli_query($con, $query);
+                $attendanceCount = mysqli_fetch_assoc($result)['attendances'];
+            
+                $query = "SELECT graduation_year FROM users LIMIT $i, 1";
+                $result = mysqli_query($con, $query);
+                $gradYear = mysqli_fetch_assoc($result)['graduation_year'];
+            
+                $query = "SELECT major FROM users LIMIT $i, 1";
+                $result = mysqli_query($con, $query);
+                $major = mysqli_fetch_assoc($result)['major'];
+            
+                echo "<p> $memberName\n NetID: $memberNetID\n attendances: $attendanceCount\n Major: $major\n grad year: $gradYear\n attendances: $attendanceCount\n\n</p>";
+            }
+            ?>
+        </tbody>
+    </table>
 
     <a href="logout.php">Logout</a>
     <h1>This is the admin page</h1>
