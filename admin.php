175f1174c4c4541a8ecd13a13df49086b06719e1
Daniel Zhang <danqzbusiness@gmail.com>
2023-03-25 23:25:44 -0400

    try1

--- admin.php
+++ admin.php
@@ -6,15 +6,23 @@ include("functions.php");
 
 $user_data = check_login($con);
 
-if (isset($_POST['submit'])) {
-    $query = "UPDATE users SET attended_current_event = 0 WHERE attended_current_event = 1";
+if (isset($_POST['startEvent'])) {
+    $query = "UPDATE users SET attended_current_event = 0 WHERE attended_current_event != 0";
     mysqli_query($con, $query);
 }
-if (isset($_POST['submit2'])) {
-    $query = "UPDATE users SET attended_current_event = 1 WHERE attended_current_event = 0";
+if (isset($_POST['endEvent'])) {
+    $query = "UPDATE users SET attended_current_event = 1 WHERE attended_current_event != 1";
     mysqli_query($con, $query);
 }
+if (isset($_POST['deleteMember'])) {
+    $targetNetID = $_POST['netID'];
+    $query = "DELETE FROM users WHERE user_name = '$targetNetID'";
+    mysqli_query($con, $query);
 
+    //refresh page
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -30,10 +38,10 @@ if (isset($_POST['submit2'])) {
     <h1>This is the admin page</h1>
 
     <form method="post">
-        <input type="submit" name="submit" value="Start Event">
+        <input type="submit" name="startEvent" value="Start Event">
     </form>
     <form method="post">
-        <input type="submit" name="submit2" value="End Event">
+        <input type="submit" name="endEvent" value="End Event">
     </form>
 
     <br>
@@ -53,9 +61,9 @@ if (isset($_POST['submit2'])) {
             $result = mysqli_query($con, $query);
             $row = mysqli_fetch_assoc($result);
             $memberCount = $row['num_users'];
-            
+
             for ($i = 0; $i < $memberCount; $i++) {
-            
+
                 // Combine first and last name
                 $query = "SELECT first_name FROM users LIMIT $i, 1";
                 $result = mysqli_query($con, $query);
@@ -64,25 +72,33 @@ if (isset($_POST['submit2'])) {
                 $result = mysqli_query($con, $query);
                 $lastName = mysqli_fetch_assoc($result)['last_name'];
                 $memberName = "$firstName $lastName";
-            
+
                 // NetID, attendance count, major, grad year
                 $query = "SELECT user_name FROM users LIMIT $i, 1";
                 $result = mysqli_query($con, $query);
                 $memberNetID = mysqli_fetch_assoc($result)['user_name'];
-            
+
                 $query = "SELECT attendances FROM users LIMIT $i, 1";
                 $result = mysqli_query($con, $query);
                 $attendanceCount = mysqli_fetch_assoc($result)['attendances'];
-            
+
                 $query = "SELECT graduation_year FROM users LIMIT $i, 1";
                 $result = mysqli_query($con, $query);
                 $gradYear = mysqli_fetch_assoc($result)['graduation_year'];
-            
+
                 $query = "SELECT major FROM users LIMIT $i, 1";
                 $result = mysqli_query($con, $query);
                 $major = mysqli_fetch_assoc($result)['major'];
-            
-                echo "<p> $memberName\n NetID: $memberNetID\n attendances: $attendanceCount\n Major: $major\n grad year: $gradYear\n attendances: $attendanceCount\n\n</p>";
+
+                $form = "post";
+                $submit = "submit";
+                $buttonName = "deleteMember";
+                $value = "Delete $memberNetID";
+
+                echo "<p>$memberName<br>NetID: $memberNetID<br>Attendance Count: $attendanceCount<br>Major: $major<br>Graduation Year: $gradYear</p>";
+                echo "<form method='$form'>";
+                echo "<button type='$submit' name='$buttonName' value='$memberNetID'>$value</button>";
+                echo "</form>";
             }
             ?>
         </tbody>
